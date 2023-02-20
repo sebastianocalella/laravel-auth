e1aee3f5f1c68a07e284e3a9963ddb4c0e11fceb
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-02-20 19:23:40 +0100

    PostController index posts list implement

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,7 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        
+    <div class="container my-5">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#id</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Author</th>
+                    <th scope="col">post date</th>
+                    <th scope="col">actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($posts as $post)
+                    <tr>
+                        <th scope="row">{{ $post->id }}</th>
+                        <td>{{ $post->title }}</td>
+                        <td>{{ $post->author }}</td>
+                        <td>{{ $post->post_date }}</td>
+                        <td>
+                            <a class="btn btn-sm btn-primary" href="">Show</a>
+                            <a class="btn btn-sm btn-success" href="">Edit</a>
+                            <a class="btn btn-sm btn-danger" href="">Delete</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
+
+
+
+
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -62,7 +62,7 @@
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ route('admin.dashboard') }}">{{__('Dashboard')}}</a>
-                                <a class="dropdown-item" href="{{ route('profile') }}">{{__('Profile')}}</a>
+                                <a class="dropdown-item" href="{{ route('profile.edit') }}">{{__('Profile')}}</a>
                                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
