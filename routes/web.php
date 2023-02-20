5d5d0fdd529fdd9b1fc2c4780d36427bfa9b9bca
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-02-20 19:03:18 +0100

    starting post controller index setup

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController as DashboardController;
+use App\Http\Controllers\Admin\PostController as AdminPostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,6 +23,7 @@ Route::get('/', function () {
 Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(
     function(){
         Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
+        Route::resource('/posts', AdminPostController::class);
     }
 );
 
