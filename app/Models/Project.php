e30a81938668e9a98b1cd10effd7f76f7b3d5e87
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-02-22 12:36:04 +0100

    create project model

---
+++ app/Models/Project.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'author',
+        'slug',
+        'title',
+        'modification_date',
+        'is_urgent',
+        'description'
+    ];
+
+    /**
+     * Get the route key for the model
+     * 
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+}
